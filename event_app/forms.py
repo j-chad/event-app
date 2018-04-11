220f6807df53b1e9289387b4a42adee2166550b0
Jackson Chadfield <chadfield.jackson@gmail.com>
2018-04-11 12:34:56 +1200

    Created forms

---
+++ event_app/forms.py
@@ -0,0 +1,69 @@
+import wtforms
+from flask_wtf import FlaskForm
+
+from .extensions import bcrypt
+from .models import User
+from .util import PasswordRules
+
+# PASSWORD SETTINGS
+p_manager = PasswordRules(
+    uppercase=2,
+    lowercase=5,
+    digits=2,
+    special=1,
+    length=10
+)
+
+
+class LoginForm(FlaskForm):
+    email = wtforms.StringField(validators=[
+        wtforms.validators.DataRequired(),
+        wtforms.validators.Email(),
+        wtforms.validators.Length(max=100)
+    ])
+    password = wtforms.PasswordField(validators=[
+        wtforms.validators.DataRequired()
+    ])
+
+    def __init__(self, *args, **kwargs):
+        # noinspection PyCallByClass
+        FlaskForm.__init__(self, *args, **kwargs)
+        self.user = None
+
+    def validate(self) -> bool:
+        rv = FlaskForm.validate(self)
+        if not rv:
+            return False
+        user = User.query.filter_by(email=self.email.data).first()
+        if user is None:
+            # Hash random password anyways in an attempt to prevent timing attack
+            bcrypt.generate_password_hash('lol random password' + 'random salt')
+            self.password.errors.append("Unknown Username Or Password")
+            return False
+        else:
+            if user.check_password(self.password.data):
+                self.user = user
+                return True
+            else:
+                self.password.errors.append("Unknown Username Or Password")
+                return False
+
+
+class RegisterForm(FlaskForm):
+    first_name = wtforms.StringField(validators=[
+        wtforms.validators.DataRequired(),
+        wtforms.validators.Length(max=40)
+    ])
+    last_name = wtforms.StringField(validators=[
+        wtforms.validators.Optional(),
+        wtforms.validators.Length(max=40)
+    ])
+    email = wtforms.StringField(validators=[
+        wtforms.validators.DataRequired(),
+        wtforms.validators.Email(),
+        wtforms.validators.Length(max=100)
+    ])
+    password = wtforms.PasswordField(validators=[
+        wtforms.validators.DataRequired(),
+        p_manager
+    ])
--- event_app/util.py
+++ event_app/util.py
@@ -1,4 +1,90 @@
+from typing import Counter, Optional
+
+import collections
+import wtforms
+from wtforms import ValidationError
+
 from .extensions import db
+
+
+class PasswordRules:
+    UPPERCASE = {
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+        'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+        'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+        'Y', 'Z'
+    }
+    LOWERCASE = {
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+        'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+        'y', 'z'
+    }
+    DIGITS = {
+        '0', '1', '2', '3', '4', '5', '6', '7',
+        '8', '9'
+    }
+
+    def __init__(self,
+                 uppercase: Optional[int] = None,
+                 lowercase: Optional[int] = None,
+                 digits: Optional[int] = None,
+                 special: Optional[int] = None,
+                 length: int = 10):
+        if uppercase + lowercase + digits + special > length:
+            raise ValueError("Length must be >= sum of requirements")
+        self.uppercase = uppercase
+        self.lowercase = lowercase
+        self.digits = digits
+        self.special = special
+        self.length = length
+
+    def validate(self, password: str, raise_error: bool = False):
+        char_count = self.count_characters(password)
+
+        def test(a: int, b: int) -> bool:
+            return (a is not None) and (a > b)
+
+        errors = []
+
+        if test(self.uppercase, char_count['uppercase']):
+            errors.append("Need at least {} uppercase character{}".format(
+                self.uppercase, 's' if self.uppercase != 1 else ''
+            ))
+        if test(self.lowercase, char_count['lowercase']):
+            errors.append("Need at least {} lowercase character{}".format(
+                self.lowercase, 's' if self.lowercase != 1 else ''
+            ))
+        if test(self.digits, char_count['digits']):
+            errors.append("Need at least {} digit{}".format(
+                self.digits, 's' if self.digits != 1 else ''
+            ))
+        if test(self.special, char_count['special']):
+            errors.append("Need at least {} special character{}".format(
+                self.special, 's' if self.special != 1 else ''
+            ))
+        if test(self.length, char_count['length']):
+            errors.append("Password must be at least {} characters".format(self.length))
+        if raise_error and errors:
+            raise ValidationError(errors[0])
+        return errors
+
+    def __call__(self, form: wtforms.Form, field: wtforms.Field):
+        return self.validate(field.data, raise_error=True)
+
+    @staticmethod
+    def count_characters(password: str) -> Counter[str]:
+        counter = collections.Counter(['uppercase', 'lowercase', 'digits', 'special'])
+        for char in password:
+            if char in PasswordRules.UPPERCASE:
+                counter["uppercase"] += 1
+            elif char in PasswordRules.LOWERCASE:
+                counter["lowercase"] += 1
+            elif char in PasswordRules.DIGITS:
+                counter["digits"] += 1
+            else:
+                counter["special"] += 1
+        return counter
 
 
 def reference_col(table: db.Model, nullable: bool = False, pk_name: str = 'id', **kwargs) -> db.Column:
